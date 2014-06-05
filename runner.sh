e8d2400ffebc2f11509cf73aaa2bdd0e6fb0257d
phil.cryer <phil@cryer.us>
2014-06-05 15:40:31 -0500

    updated with grub fixing goodness

--- runner.sh
+++ runner.sh
@@ -4,13 +4,18 @@ set -e
 
 baseurl="https://raw.githubusercontent.com/philcryer/tonka32/"
 
+echo " *** starting ***"
+
+sleep 1
+
 echo " *** checking permissions"
 if [[ $EUID -ne 0 ]]; then
 	echo "	--- FAIL ---- This script must be run as root (you can trust me, right?)" 1>&2
 	exit 1
 fi
 
-echo " *** update package cache"
+
+echo " *** updating package cache"
 apt-get -yy update
 
 echo " *** install curl if it's not already installed"
@@ -18,26 +23,28 @@ if [ ! -f '/usr/bin/curl' ]; then
 	apt-get -yy install curl
 fi
 
-echo " *** create a basic firewall, only allowing 22 by default"
+echo " *** creating a *basic* firewall, only allowing 22 by default"
 if [ ! -f '/etc/init.d/firewall' ]; then
      	curl $baseurl/master/bin/firewall -o /etc/init.d/firewall
 	chmod 755 /etc/init.d/firewall
-	#curl $baseurl/master/etc/firewall.conf -o /etc/firewall.conf
 	update-rc.d firewall defaults
 fi
 
-echo " *** install/verify we have needed software (an error is expected!)"
+echo " *** installing/verifying we have needed software (an error wouldn't be unexpected!)"
 echo "     (libpam-tmpdir, libpam-cracklib, apparmor-profiles, ntp, openssh-server)"
 apt-get -yy install libpam-tmpdir libpam-cracklib apparmor-profiles ntp openssh-server
 
-echo " *** fix the error by putting apparmor in grub and rebooting"
 if [ `grep "security=apparmor" /etc/default/grub; echo $?` == '1' ]; then
+	echo " *** fix the error by putting apparmor line in grub and rebooting"
 	sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="/&security=apparmor /' /etc/default/grub
 	update-grub
-	echo "sh /tmp/runner.sh" > /etc/rc.local
+	sed -i -e 's/exit 0//' /etc/rc.local
+	echo "cd /tmp; ./runner.sh" >> /etc/rc.local
+	echo "exit 0" >> /etc/rc.local
 	echo " *** rebooting"
-	#/sbin/reboot
- fi
+	/sbin/reboot
+fi
 
 
 exit 0
+
